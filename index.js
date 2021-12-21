0a2ef6f455459147f9f25f868b3ef369d666a5a0
Stevie Gill <29311273+caffeinated-pixels@users.noreply.github.com>
2021-12-21 09:50:16 -0500

    Calculate speed on location change

--- index.js
+++ index.js
@@ -1,11 +1,11 @@
 const prevLocationDisplay = document.getElementById('prev-location')
 const distanceDisplay = document.getElementById('distance')
-const speedometer = document.getElementById('speedometer')
 const select = document.getElementById('select')
 const time = document.getElementById('time')
+const speedDisplay = document.getElementById('speed-display')
 
 select.addEventListener('change', (e) => setPrevLocation(e.target.value))
-time.addEventListener('change', calculateSpeed)
+time.addEventListener('change', (e) => calculateSpeed(e.target.value))
 
 let prevLocation = {}
 let timeTaken = 0
@@ -51,23 +51,24 @@ function setPrevLocation(currentLocation) {
     currentIndex === 0 ? destination.length - 1 : currentIndex - 1
   prevLocation = destination[previousIndex]
   displayPreviousLocation()
+  calculateSpeed(time.value)
 }
 
-setPrevLocation(select.value)
-
-function calculateSpeed(e) {
-  const time = e.target.value
+function calculateSpeed(time) {
   const distance = prevLocation.distanceFromPrevDestination
 
   const speed = Math.round(distance / time)
 
-  console.log(speed)
-
-  // Task:
-  // - Retrieve distance from previous destination from array of objects.
-  // - Calculate speed and round speed to an integer (whole number).
-  // - Display speed in speedometer paragraph.
+  speedDisplay.textContent = `${speed} knots`
 }
+
+setPrevLocation(select.value)
+calculateSpeed(time.value)
+
+// Task:
+// - Retrieve distance from previous destination from array of objects.
+// - Calculate speed and round speed to an integer (whole number).
+// - Display speed in speedometer paragraph.
 
 // Stretch goals:
 // - Calculate average overall speed.
